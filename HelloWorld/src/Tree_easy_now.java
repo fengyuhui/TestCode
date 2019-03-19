740a89329a8c7a4db1533b672a1b90cc9f5c1676
fengyuhui <1427094777@qq.com>
2019-03-19 12:25:49 +0800

    Update Tree_easy_now.java
    101 对称二叉树

--- HelloWorld/src/Tree_easy_now.java
+++ HelloWorld/src/Tree_easy_now.java
@@ -12,6 +12,7 @@ public class Tree_easy_now {
         return;
     }
 
+    //100 相同的树
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null || q == null){
             if(p == null && q == null)
@@ -23,6 +24,37 @@ public class Tree_easy_now {
                 return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
             }else
                 return false;
+        }
+    }
+
+    //101对称二叉树
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        if(root.left == null || root.right == null){
+            if(root.left == null && root.right == null) {
+                return true;
+            }
+            else
+                return false;
+        }else{
+            return isSymmetricSub(root.left, root.right);
+        }
+    }
+
+    public boolean isSymmetricSub(TreeNode l, TreeNode r){
+        if(l == null || r == null){
+            if(l == null && r == null) {
+                return true;
+            }
+            else
+                return false;
+        }else{
+            if(l.val!=r.val)
+                return false;
+            else {
+                return isSymmetricSub(l.left, r.right)&&isSymmetricSub(l.right, r.left);
+            }
         }
     }
 }

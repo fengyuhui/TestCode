1d054a2ed588380a6b527980fce9c24fc53e10d5
fengyuhui <1427094777@qq.com>
2019-03-19 11:08:06 +0800

    Update Tree_easy_now.java
    100 相同的树

--- HelloWorld/src/Tree_easy_now.java
+++ HelloWorld/src/Tree_easy_now.java
@@ -1,2 +1,28 @@
+// Definition for a binary tree node.
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+
 public class Tree_easy_now {
+    public static void main(String args[]){
+        return;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null){
+            if(p == null && q == null)
+                return true;
+            else
+                return false;
+        }else{
+            if(p.val == q.val){
+                return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+            }else
+                return false;
+        }
+    }
 }

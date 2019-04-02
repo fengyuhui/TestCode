19c4c4ef67b16a610a29ffce7adc9f132858c974
fengyuhui <1427094777@qq.com>
2019-04-02 13:40:39 +0800

    快排

--- HelloWorld/src/medium/Array_Medium_notComplete.java
+++ HelloWorld/src/medium/Array_Medium_notComplete.java
@@ -6,6 +6,10 @@ public class Array_Medium_notComplete {
         int[] height = {1,8,6,2,5,4,8,3,7};
         main.maxArea(height);
 
+        int[] sor = {0,6,1,2,7,9,3,4,5,10,8};
+        main.quickSort(sor);
+        System.out.println(sor);
+
     }
 
     //11. 盛最多水的容器，当然这个是有复杂度n的最优解，现在用的是暴力复杂度n^2的解
@@ -61,6 +65,53 @@ public class Array_Medium_notComplete {
             }
         }
         return matrix;
+    }
+
+    //快排
+    /*******************************************************
+     *快速排序 (比较排序类)
+     *每次排序将待排记录分割两部分,一部分都比关键字小,一部分都比关键字大。这个快排是要预留第一位为0的！！！！
+     ********/
+    public void quickSort(int[] L) {
+        Qsort(L,1,L.length-1);
+    }
+
+    public void Qsort(int[] L,int low,int high) {
+        int pivot;
+        if(low<high) {
+            //将L[low,high]一分为二,算出枢轴值pivot,该值得位置固定,不用再变化
+            pivot=partition0(L,low,high);
+
+            //对两边的数组分别排序
+            Qsort(L,low,pivot-1);
+            Qsort(L,pivot+1,high);
+        }
+    }
+
+    //  选择一个枢轴值(关键字) 把它放到某个位置 使其左边的值都比它小 右边的值都比它大
+    public int partition0(int[] L,int low,int high) {
+        int pivotkey;
+        pivotkey=L[low];
+        //顺序很重要，要先从右边找
+        while(low<high) {
+            while(low<high && L[high]>=pivotkey) {  //从后往前找到比key小的放到前面去
+                high--;
+            }
+            swap(L,low,high);
+            while(low<high && L[low]<=pivotkey) {  //从前往后找到比key大的 放到后面去
+                low++;
+            }
+            swap(L,low,high);
+        } //遍历所有记录  low的位置即为 key所在位置, 且固定,不用再改变
+        return low;
+    }
+    //交换数组的两个位置
+    public void swap(int[] L,int i,int j) {
+
+        int temp=L[i];
+        L[i]=L[j];
+        L[j]=temp;
+
     }
 
 

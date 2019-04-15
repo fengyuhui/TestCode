be903fdaa8929d97c6754032413303163ea8efc4
fengyuhui <1427094777@qq.com>
2019-04-15 18:11:32 +0800

    BFS

--- HelloWorld/src/medium/BFS_Medium_notComplete.java
+++ HelloWorld/src/medium/BFS_Medium_notComplete.java
@@ -4,6 +4,12 @@ import java.util.*;
 
 public class BFS_Medium_notComplete {
     public  static void main(String[] args){
+        BFS_Medium_notComplete main = new BFS_Medium_notComplete();
+        int[][] p = {{1, 0}, {1, 2}, {1, 3}};
+        //main.findMinHeightTrees(4,p);
+        int times[][] = {{2,1,1},{2,3,1},{3,4,1}};
+        main.networkDelayTime(times, 4,2);
+
 
     }
 
@@ -68,11 +74,61 @@ public class BFS_Medium_notComplete {
 
     //909. 蛇梯棋
 
-    
+
     //310. 最小高度树
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        List<Integer> ans = new ArrayList<>();
 
+        List<Integer> ans = new ArrayList<>();
+        if(n == 1) {
+            ans.add(0);
+            return ans;
+        }
+        //可以把每个节点都当作root, 然后dfs来查找每棵树的高度，选最小的
+        //还可以挨个删除叶节点，也就是度为1的节点(因为这个不是有向图，所以度既是出度也是入度)，然后再将依赖的头节点的入度-1，最后剩下的一个或两个节点就是所求
+        //初始化度的数组
+
+        int[] degree = new int[n];
+        for(int[] edge:edges){
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+        //存放度为1的节点
+        int nums = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i<n; i++){
+            if(degree[i] == 1) {
+                queue.add(i);
+                nums++;
+            }
+        }
+        while(!queue.isEmpty() && nums<n) {
+            int size = queue.size();
+            while (size > 0) {
+                int temp = queue.poll();
+                size--;
+
+                //删除对应的依赖度
+                for (int[] edge : edges) {
+                    if (edge[0] == temp) {
+                        degree[edge[1]]--;
+                        if (degree[edge[1]] == 1) {
+                            queue.add(edge[1]);
+                            nums++;
+                        }
+                    }
+                    if (edge[1] == temp) {
+                        degree[edge[0]]--;
+                        if (degree[edge[0]] == 1) {
+                            queue.add(edge[0]);
+                            nums++;
+                        }
+                    }
+                }
+            }
+        }
+        while(!queue.isEmpty()){
+            ans.add(queue.poll());
+        }
         return ans;
     }
 
@@ -107,6 +163,68 @@ public class BFS_Medium_notComplete {
             ans.add(tempList);
         }
         return ans;
+    }
+
+    //743. 网络延迟时间，dijkstra算法，最后是否有节点剩下，如果有就不能达到
+    public int networkDelayTime(int[][] times, int N, int K) {
+        Set<Integer> visited = new HashSet<>();
+        //value[i]表示K到i的最小时间
+        int[] value = new int[N+1];
+        //初始化value
+        for(int i = 0; i<N+1; i++){
+            value[i] = 1000000;
+        }
+        //自身节点时间为0
+        value[K] = 0;
+        //waste[i][j]代表节点i到节点j的直接时间
+        int[][] waste = new int[N+1][N+1];
+        //初始化waste
+        for(int i = 1; i<=N; i++){
+            for(int j = 1; j<=N; j++){
+                if(i!=j)
+                    waste[i][j] = 1000000;
+            }
+        }
+        for(int[] time:times){
+            waste[time[0]][time[1]] = time[2];
+            if(time[0] == K){
+                value[time[1]] = time[2];
+            }
+        }
+
+
+        while(true){
+            int index = findMinNext(visited, N, value);
+            if(value[index] == value[0])
+                break;
+            visited.add(index);
+            for(int i = 1; i<=N; i++){
+                if(value[index]>value[i]+waste[i][index])
+                    value[index] = value[i]+waste[i][index];
+            }
+        }
+
+        int max = 0;
+        if(visited.size() == N){
+            for(int i = 1; i<=N; i++){
+                max = max<value[i]?value[i]:max;
+            }
+            return max;
+        }
+        return -1;
+    }
+    public int findMinNext(Set<Integer> visited,int N, int[] value){
+        int index = 1;
+        int min = value[0];
+        for(int i = 1; i<=N; i++){
+            if(visited.contains(i))
+                continue;
+            if(min>value[i]) {
+                min = value[i];
+                index = i;
+            }
+        }
+        return index;
     }
 
 }
